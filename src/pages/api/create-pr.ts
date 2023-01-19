d79ab793ec27a80229db8aaacd722e5e41a0f1aa
kyllolive <kyle.cuizon4@gmail.com>
2023-01-19 09:24:59 +0800

    test

--- src/pages/api/create-pr.ts
+++ src/pages/api/create-pr.ts
@@ -1,288 +1,288 @@
-import fs from "fs";
-import { Octokit } from "octokit";
-import { createPullRequest } from "octokit-plugin-create-pull-request";
-import ts from "typescript";
-import { ObjectLiteralExpression, Project } from "ts-morph";
+// import fs from "fs";
+// import { Octokit } from "octokit";
+// import { createPullRequest } from "octokit-plugin-create-pull-request";
+// import ts from "typescript";
+// import { ObjectLiteralExpression, Project } from "ts-morph";
 
-export default async function handler(req, res) {
-  const { languageID, namespace, items } = req.body;
+// export default async function handler(req, res) {
+//   const { languageID, namespace, items } = req.body;
 
-  const project = new Project();
+//   const project = new Project();
 
-  const MyOctokit = Octokit.plugin(createPullRequest);
+//   const MyOctokit = Octokit.plugin(createPullRequest);
 
-  const octokit = new MyOctokit({
-    auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
-  });
+//   const octokit = new MyOctokit({
+//     auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+//   });
 
-  //list of spread operator assignments
-  let importedFiels = [];
+//   //list of spread operator assignments
+//   let importedFiels = [];
 
-  // get list of files in directory
-  // const sourceFile = project.addSourceFileAtPathIfExists(
-  //   `src/translations/locales/${languageID}/${namespace}.ts`
-  // );
+//   // get list of files in directory
+//   // const sourceFile = project.addSourceFileAtPathIfExists(
+//   //   `src/translations/locales/${languageID}/${namespace}.ts`
+//   // );
 
-  //
-  const sourceFileFromGithub = await octokit.request(
-    "GET /repos/{owner}/{repo}/contents/{path}",
-    {
-      owner: "kyllolive",
-      repo: "nextjs-octokit-demo",
-      path: `src/translations/locales/${languageID}/${namespace}.ts`,
-    }
-  );
+//   //
+//   const sourceFileFromGithub = await octokit.request(
+//     "GET /repos/{owner}/{repo}/contents/{path}",
+//     {
+//       owner: "kyllolive",
+//       repo: "nextjs-octokit-demo",
+//       path: `src/translations/locales/${languageID}/${namespace}.ts`,
+//     }
+//   );
 
-  // convert content to utf8
-  const content = Buffer.from(
-    sourceFileFromGithub.data["content"],
-    "base64"
-  ).toString("utf8");
+//   // convert content to utf8
+//   const content = Buffer.from(
+//     sourceFileFromGithub.data["content"],
+//     "base64"
+//   ).toString("utf8");
 
-  const addSourceFile = project.addSourceFileAtPathIfExists(
-    sourceFileFromGithub.data["path"]
-  );
+//   const addSourceFile = project.addSourceFileAtPathIfExists(
+//     sourceFileFromGithub.data["path"]
+//   );
 
-  // console.log(addSourceFile);
+//   // console.log(addSourceFile);
 
-  //get list of object literals in file
+//   //get list of object literals in file
 
-  const propertyToUpdate =
-    addSourceFile.getVariableDeclarationOrThrow(namespace);
+//   const propertyToUpdate =
+//     addSourceFile.getVariableDeclarationOrThrow(namespace);
 
-  let objectLiteralExpression =
-    propertyToUpdate.getInitializer() as ObjectLiteralExpression;
+//   let objectLiteralExpression =
+//     propertyToUpdate.getInitializer() as ObjectLiteralExpression;
 
-  console.log(objectLiteralExpression);
+//   console.log(objectLiteralExpression);
 
-  // if (items.length > 0) {
-  //   if (sourceFile) {
-  //     items.forEach(async (item) => {
-  //       if (!item.translationKey) {
-  //         return;
-  //       }
+//   // if (items.length > 0) {
+//   //   if (sourceFile) {
+//   //     items.forEach(async (item) => {
+//   //       if (!item.translationKey) {
+//   //         return;
+//   //       }
 
-  //       objectLiteralExpression.getPropertyOrThrow(item.translationKey).set({
-  //         initializer: `'${item.translationValue}'`,
-  //       });
+//   //       objectLiteralExpression.getPropertyOrThrow(item.translationKey).set({
+//   //         initializer: `'${item.translationValue}'`,
+//   //       });
 
-  //       const modifiedCode = sourceFile.getText();
+//   //       const modifiedCode = sourceFile.getText();
 
-  //       if (modifiedCode) {
-  //         //check if pull request already exists
-  //         const { data: pullRequests } = await octokit.request(
-  //           "GET /repos/{owner}/{repo}/pulls",
-  //           {
-  //             owner: "kyllolive",
-  //             repo: "nextjs-octokit-demo",
-  //             state: "open",
-  //             head: `update-translation`,
-  //           }
-  //         );
-  //         // const result = await octokit.createPullRequest({
-  //         //   owner: "kyllolive",
-  //         //   repo: "nextjs-octokit-demo",
-  //         //   title: "Update translation",
-  //         //   body: "Update translation",
-  //         //   head: "update-translation",
-  //         //   base: "main",
-  //         //   update: pullRequests[0]?.state === "open" ? true : false,
-  //         //   changes: [
-  //         //     {
-  //         //       files: {
-  //         //         [`src/translations/locales/${languageID}/${namespace}.ts`]:
-  //         //           modifiedCode,
-  //         //       },
-  //         //       commit: "Update translation",
-  //         //     },
-  //         //   ],
-  //         // });
+//   //       if (modifiedCode) {
+//   //         //check if pull request already exists
+//   //         const { data: pullRequests } = await octokit.request(
+//   //           "GET /repos/{owner}/{repo}/pulls",
+//   //           {
+//   //             owner: "kyllolive",
+//   //             repo: "nextjs-octokit-demo",
+//   //             state: "open",
+//   //             head: `update-translation`,
+//   //           }
+//   //         );
+//   //         // const result = await octokit.createPullRequest({
+//             // owner: "kyllolive",
+//             // repo: "nextjs-octokit-demo",
+//             // title: "Update translation",
+//             // body: "Update translation",
+//             // head: "update-translation",
+//             // base: "main",
+//             // update: pullRequests[0]?.state === "open" ? true : false,
+//   //         //   changes: [
+//   //         //     {
+//   //         //       files: {
+//   //         //         [`src/translations/locales/${languageID}/${namespace}.ts`]:
+//   //         //           modifiedCode,
+//   //         //       },
+//   //         //       commit: "Update translation",
+//   //         //     },
+//   //         //   ],
+//   //         // });
 
-  //         // console.log("Done!", result);
-  //       }
-  //     });
-  //   }
-  // }
+//   //         // console.log("Done!", result);
+//   //       }
+//   //     });
+//   //   }
+//   // }
 
-  //loop through imported files and search for the object literal
-  //   for (const importedFile of importedFiles) {
-  //     const importedFileObjectLiteral = importedFile
-  //       .getModuleSpecifierSourceFile()
-  //       .getVariableDeclarationOrThrow(namespace)
-  //       .getInitializer() as ObjectLiteralExpression;
-  //     try {
-  //       importedFileObjectLiteral
-  //         .getPropertyOrThrow(item.translationKey)
-  //         .set({ initializer: `'${item.translationValue}'` });
-  //     } catch (error) {
-  //       if (error.name === "InvalidOperationError") {
-  //         continue;
-  //       }
-  //     }
-  //   }
-  // }else {
+//   //loop through imported files and search for the object literal
+//   //   for (const importedFile of importedFiles) {
+//   //     const importedFileObjectLiteral = importedFile
+//   //       .getModuleSpecifierSourceFile()
+//   //       .getVariableDeclarationOrThrow(namespace)
+//   //       .getInitializer() as ObjectLiteralExpression;
+//   //     try {
+//   //       importedFileObjectLiteral
+//   //         .getPropertyOrThrow(item.translationKey)
+//   //         .set({ initializer: `'${item.translationValue}'` });
+//   //     } catch (error) {
+//   //       if (error.name === "InvalidOperationError") {
+//   //         continue;
+//   //       }
+//   //     }
+//   //   }
+//   // }else {
 
-  // const properties = propertyToUpdate
-  //   .getFirstDescendantByKind(ts.SyntaxKind.ObjectLiteralExpression)
-  //   .getProperties();
+//   // const properties = propertyToUpdate
+//   //   .getFirstDescendantByKind(ts.SyntaxKind.ObjectLiteralExpression)
+//   //   .getProperties();
 
-  // for (const property of properties) {
-  //   if (ts.isSpreadAssignment(property.compilerNode)) {
-  //     const identifier = property.compilerNode
-  //       .expression as ts.Identifier;
-  //     console.log(identifier.text);
-  //     const importDeclaration = sourceFile.getImportDeclaration(
-  //       `./commons/${identifier.text}`
-  //     );
+//   // for (const property of properties) {
+//   //   if (ts.isSpreadAssignment(property.compilerNode)) {
+//   //     const identifier = property.compilerNode
+//   //       .expression as ts.Identifier;
+//   //     console.log(identifier.text);
+//   //     const importDeclaration = sourceFile.getImportDeclaration(
+//   //       `./commons/${identifier.text}`
+//   //     );
 
-  //     console.log(importDeclaration);
-  //   }
-  // }
+//   //     console.log(importDeclaration);
+//   //   }
+//   // }
 
-  // let objectLiteralExpression =
-  //   propertyToUpdate.getInitializer() as ObjectLiteralExpression;
+//   // let objectLiteralExpression =
+//   //   propertyToUpdate.getInitializer() as ObjectLiteralExpression;
 
-  // const test = propertyToUpdate.getText();
-  // console.log("test", test);
+//   // const test = propertyToUpdate.getText();
+//   // console.log("test", test);
 
-  //only set initializer if it's not the same as the existing one
-  // if (
-  //   objectLiteralExpression
-  //     .getPropertyOrThrow(item.translationKey)
-  //     .getText() === item.translationValue
-  // ) {
-  //   return;
-  // }
+//   //only set initializer if it's not the same as the existing one
+//   // if (
+//   //   objectLiteralExpression
+//   //     .getPropertyOrThrow(item.translationKey)
+//   //     .getText() === item.translationValue
+//   // ) {
+//   //   return;
+//   // }
 
-  // objectLiteralExpression.getPropertyOrThrow(item.translationKey).set({
-  //   initializer: item.translationValue,
-  // });
+//   // objectLiteralExpression.getPropertyOrThrow(item.translationKey).set({
+//   //   initializer: item.translationValue,
+//   // });
 
-  // const modifiedCode = sourceFile.getText();
+//   // const modifiedCode = sourceFile.getText();
 
-  // get spread operator assignment
+//   // get spread operator assignment
 
-  // const test = objectLiteralExpression.getPropertyOrThrow((p) => {
-  //   console.log(p);
-  //   return p.getText() === "...topNav";
-  // });
+//   // const test = objectLiteralExpression.getPropertyOrThrow((p) => {
+//   //   console.log(p);
+//   //   return p.getText() === "...topNav";
+//   // });
 
-  // if (objectLiteralExpression.getProperty(item.translationKey)) {
-  //check if translationValue is not the same as the existing one
-  // if (
-  //   objectLiteralExpression
-  //     .getPropertyOrThrow(item.translationKey)
-  //     .getText() === item.translationValue
-  // ) {
-  //   return;
-  // }
+//   // if (objectLiteralExpression.getProperty(item.translationKey)) {
+//   //check if translationValue is not the same as the existing one
+//   // if (
+//   //   objectLiteralExpression
+//   //     .getPropertyOrThrow(item.translationKey)
+//   //     .getText() === item.translationValue
+//   // ) {
+//   //   return;
+//   // }
 
-  //   objectLiteralExpression.getPropertyOrThrow(
-  //     item.translationKey
-  //   ).set({
-  //     initializer: item.translationValue,
-  //   });
-  // } else {
-  //   objectLiteralExpression.addPropertyAssignment({
-  //     name: item.translationKey,
-  //     initializer: item.translationValue,
-  //   });
-  // }
+//   //   objectLiteralExpression.getPropertyOrThrow(
+//   //     item.translationKey
+//   //   ).set({
+//   //     initializer: item.translationValue,
+//   //   });
+//   // } else {
+//   //   objectLiteralExpression.addPropertyAssignment({
+//   //     name: item.translationKey,
+//   //     initializer: item.translationValue,
+//   //   });
+//   // }
 
-  // objectLiteralExpression.getPropertyOrThrow("LABEL_SIGN_UP").set({
-  //   initializer: "Sign Up!",
-  // });
+//   // objectLiteralExpression.getPropertyOrThrow("LABEL_SIGN_UP").set({
+//   //   initializer: "Sign Up!",
+//   // });
 
-  // const modifiedCode = sourceFile.getText();
+//   // const modifiedCode = sourceFile.getText();
 
-  // if (sourceFile) {
-  //   const propertyToUpdate =
-  //     sourceFile.getVariableDeclarationOrThrow(namespace);
+//   // if (sourceFile) {
+//   //   const propertyToUpdate =
+//   //     sourceFile.getVariableDeclarationOrThrow(namespace);
 
-  //   console.log(propertyToUpdate);
-  // }
+//   //   console.log(propertyToUpdate);
+//   // }
 
-  // Read the file
-  // fs.readFile("src/translations/locales/en/about.ts", "utf8", (err, data) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
+//   // Read the file
+//   // fs.readFile("src/translations/locales/en/about.ts", "utf8", (err, data) => {
+//   //   if (err) {
+//   //     console.error(err);
+//   //     return;
+//   //   }
 
-  // Parse the file with the TypeScript compiler
-  // const sourceFile = ts.createSourceFile(
-  //   "./account.ts",
-  //   data,
-  //   ts.ScriptTarget.Latest,
-  //   true
-  // );
+//   // Parse the file with the TypeScript compiler
+//   // const sourceFile = ts.createSourceFile(
+//   //   "./account.ts",
+//   //   data,
+//   //   ts.ScriptTarget.Latest,
+//   //   true
+//   // );
 
-  // console.log(sourceFile);
+//   // console.log(sourceFile);
 
-  // Convert the updated ast back to code
-  // const output = ts.createPrinter().printFile(sourceFile);
+//   // Convert the updated ast back to code
+//   // const output = ts.createPrinter().printFile(sourceFile);
 
-  // Write the modified code to a variable
-  // or you can save it back to file using `fs.writeFile`
+//   // Write the modified code to a variable
+//   // or you can save it back to file using `fs.writeFile`
 
-  // const getFile = fs.readFileSync(
-  //   "src/translations/locales/en/about.ts",
-  //   "utf8"
-  // );
+//   // const getFile = fs.readFileSync(
+//   //   "src/translations/locales/en/about.ts",
+//   //   "utf8"
+//   // );
 
-  // traverse folder based on req.body.languageID
+//   // traverse folder based on req.body.languageID
 
-  // copy content of file to new file
+//   // copy content of file to new file
 
-  // get all req.body.items translationKey and translationValue and update the new file
+//   // get all req.body.items translationKey and translationValue and update the new file
 
-  //submit new file as PR to epico-bot branch targeting the repository translations folder
+//   //submit new file as PR to epico-bot branch targeting the repository translations folder
 
-  // const octokit_result = await octokit.createPullRequest({
-  //   owner: "kyllolive",
-  //   repo: "nextjs-octokit-demo",
-  //   title: "update translations",
-  //   body: "update translations",
-  //   head: "epico-bot",
-  //   base: "main",
-  //   changes: [
-  //     {
-  //       files: {
-  //         // [`translations/${languageID}.json`]: content,
-  //         [`translations/${languageID}.json`]: ({
-  //           exists,
-  //           encoding,
-  //           content,
-  //         }) => {
-  //           // do not create the file if it does not exist
-  //           if (!exists) return null;
+//   // const octokit_result = await octokit.createPullRequest({
+//   //   owner: "kyllolive",
+//   //   repo: "nextjs-octokit-demo",
+//   //   title: "update translations",
+//   //   body: "update translations",
+//   //   head: "epico-bot",
+//   //   base: "main",
+//   //   changes: [
+//   //     {
+//   //       files: {
+//   //         // [`translations/${languageID}.json`]: content,
+//   //         [`translations/${languageID}.json`]: ({
+//   //           exists,
+//   //           encoding,
+//   //           content,
+//   //         }) => {
+//   //           // do not create the file if it does not exist
+//   //           if (!exists) return null;
 
-  //           return Buffer.from(content, encoding)
-  //             .toString("utf-8")
-  //             .toUpperCase();
-  //         },
-  //       },
-  //       commit: "update translations",
-  //     },
-  //   ],
-  // });
+//   //           return Buffer.from(content, encoding)
+//   //             .toString("utf-8")
+//   //             .toUpperCase();
+//   //         },
+//   //       },
+//   //       commit: "update translations",
+//   //     },
+//   //   ],
+//   // });
 
-  // .res
-  //   .status(200)
-  //   .json({ name: req.body });
-  res.status(200).json({ name: "pullRequest" });
+//   // .res
+//   //   .status(200)
+//   //   .json({ name: req.body });
+//   res.status(200).json({ name: "pullRequest" });
 
-  //Traverse the AST and find the property
-  // function findProperty(node) {
-  //   if (ts.isPropertyAssignment(node)) {
-  //     console.log("test", node);
-  //     const name = node.name;
-  //     // console.log("test", name.getText());
-  //     if (name.getText() === "LABEL_SIGN_IN") {
-  //       // Update the value of the property
-  //       node.initializer = ts.createStringLiteral("Sign in");
-  //     }
-  //   }
-  //   ts.forEachChild(node, findProperty);
-  // }
-}
+//   //Traverse the AST and find the property
+//   // function findProperty(node) {
+//   //   if (ts.isPropertyAssignment(node)) {
+//   //     console.log("test", node);
+//   //     const name = node.name;
+//   //     // console.log("test", name.getText());
+//   //     if (name.getText() === "LABEL_SIGN_IN") {
+//   //       // Update the value of the property
+//   //       node.initializer = ts.createStringLiteral("Sign in");
+//   //     }
+//   //   }
+//   //   ts.forEachChild(node, findProperty);
+//   // }
+// }
